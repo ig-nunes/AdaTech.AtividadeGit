9f8d0f461d0bb6a9190761d4896c07c8803d13ef
daisyr <daisyr@ME003504>
2023-12-13 15:54:00 -0300

    Add Daisy.cs Class

--- Principal/Program.cs
+++ Principal/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var daisy = new Daisy();
+            daisy.Says("Olá, Mundo");
         }
     }
 }
