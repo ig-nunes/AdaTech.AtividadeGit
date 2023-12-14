8d5a381a533cd621b06bc8d22a677482ee83e7b5
Monalisa Freitas de Brito <monalisabrito.ads@gmail.com>
2023-12-14 18:10:51 -0300

    Adicionar coordenadores

---
+++ Principal/Coordenadores.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Principal
+{
+    internal class Coordenadores
+    {
+        public string Id { get; set; }
+        public string Nome { get; set;}
+        public double TempoDeTrabalhoNaEmpresa { get; set; }
+        public string Turno { get; set; }
+    }
+}
